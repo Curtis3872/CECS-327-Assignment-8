56266b148c10f944f1ee9678cd4a69bde3ed6e9d
Idaky <84680451+Idakyyyy@users.noreply.github.com>
2024-11-23 21:11:17 -0800

    Step 1_1 server-side initialization
    On the server side, it is obtaining the information from mongodb and collecting it to be used later to pass to the client

--- tcp_server.py
+++ tcp_server.py
@@ -4,6 +4,20 @@ import socket
 #Import the MongoDB client
 from pymongo import MongoClient
 
+#Use for 3 hours ago
+from datetime import datetime, timedelta
+three_hours_ago = datetime.utcnow() - timedelta(hours=3)
+
+#MongoDB URL
+client = MongoClient("mongodb+srv://peksonmichael:EgGiNZRLzTN581tP@cluster0.y9w6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+#Get the database directly and put into dataset_1
+dataset_1 = client["test"]
+mycol= dataset_1["Dataset_1_virtual"]
+
+# Query the collection in the last three hours
+query = {"time": {"$gte": three_hours_ago}}
+
 #Find the way to change to use sockets in a certain way, have prints for IP and port number and remembr the inputs for it too.
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = input("Enter the IP address to bind ('0.0.0.0' to listen to all): ")
